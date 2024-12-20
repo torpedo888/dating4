98728b9c7826a03d1273efb5885876c2f16c757b
Ferenc Batorligeti <ferenc.batorligeti@capgemini.com>
2024-12-20 10:17:05 +0100

    Routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,20 +11,20 @@ import { AccountService } from '../_services/account.service';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  private accountService = inject(AccountService)
+  private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
 
   model: any = {};
 
   register() {
     this.accountService.register(this.model).subscribe({
-      next: response => {
+      next: (response) => {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
-      
-    })
+      error: (error) => this.toastr.error(error.error),
+    });
   }
 
   cancel() {
