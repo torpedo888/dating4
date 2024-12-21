45ce94132009abc2f90112bc3927726a160bd6cd
Ferenc Batorligeti <ferenc.batorligeti@capgemini.com>
2024-12-21 19:08:02 +0100

    Error Handling

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using API.Data;
 using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -24,10 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 
-// app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-//     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseCors(x => x.AllowAnyHeader()
                   .AllowAnyMethod()
                   .WithOrigins("http://localhost:4200")
